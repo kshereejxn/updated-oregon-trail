71cfc37ee25e6660e235a63d9bf1a05c6f34ed8f
Trinie J <jxsn00ks@gmail.com>
2020-07-26 16:03:38 -0400

    finished

--- main.js
+++ main.js
@@ -36,14 +36,14 @@ class Traveler {
         this.isHealthy = true
     }
     hunt(){
-        this.food === 2
+        this.food += 2
     }
     eat(){
         if (this.food <= 0){
-            return this.isHealthy === false
+            return this.isHealthy = false
         }
         else{
-            this.food += 1
+            this.food -= 1
         }
         
     }
@@ -53,13 +53,8 @@ class Doctor extends Traveler{
         super (name, isHealthy) 
                 this.isHealthy = true
 }
-heal (){
-    if (this.heal === true){
-        return this.isHealthy 
-    }
-    else{
-        
-    }
+heal (traveler){
+    traveler.isHealthy = true
 }
 
 //heal(traveler) - set the traveler's isHealthy property to true.
@@ -68,23 +63,31 @@ heal (){
 class Hunter extends Traveler{
     constructor (name, hunt, eat){
         super (name, hunt, eat)
+                this.food = 2
         
     }
     hunt(){
-        this.food ===5
+        this.food += 5
     }
 //hunt() - Increase the hunter's food by 5. (A normal traveler gains only 2.)
     eat(){
         if (this.food <= 1){
-            return this.isHealthy === false
+            this.food -= this.food
+            return this.isHealthy = false
         }
         else {
-            this.food += 2
+            this.food -= 2
             
         }
     }
 //eat() - Consumes 2 units of food. If the hunter doesn't have 2 food when they are instructed to eat, they eat as much as they can (0 or 1 unit), but the hunter is no longer healthy.
-  
+  giveFood(traveler, numOfFoodUnits){
+      if (this.food >= numOfFoodUnits){
+          this.food -= numOfFoodUnits
+          traveler.food += numOfFoodUnits
+      }
+      
+  }
 //giveFood(traveler, numOfFoodUnits) - Transfers numOfFoodUnits from the hunter to the traveler. If the hunter doesn't have enough food, then no food should be transferred.
 }
 
@@ -94,26 +97,18 @@ class Wagon {
         this.passengersList = []
     }
     getAvailableSeatCount(){
-       return (this.capacity - this.passengersList.length)
+       return this.capacity - this.passengersList.length
         /*Returns the number of empty seats, determined by the capacity set when the wagon was created, subtracted by the number of passengers currently on board.*/
     }
     join(traveler){
-        if (this.getAvailableSeatCount <=0){
-            return 'At Max Capacity'
-        }
-        else{
-            this.passengersList.push [traveler]
+        if (this.getAvailableSeatCount() > 0){
+            this.passengersList.push (traveler)
         }
         /*Adds the traveler to the wagon if there is space. If the wagon is already at maximum capacity, don't add them.*/
     }
     shouldQuarantine(){
         let unhealthy = this.passengersList.some (traveler => traveler.isHealthy === false)
-        if (unhealthy === false){
-            return 'true'
-        }
-        else{
-            return 'false'
-        }
+        return unhealthy
         /*Returns true if there is at least one unhealthy person in the wagon. Return false if not.*/
     }
     totalFood (){
@@ -121,6 +116,7 @@ class Wagon {
         for (let index = 0; index < this.passengersList.length; index += 1){
             amountFood += this.passengersList[index].food
         }
+        return amountFood
         //Returns the total amount of food among all passengers in the wagon.
     }
 }
